0abe78aca1a232460ca0393f8dd57f1bb532bdbe
Kyle Custodio <kkzyc@yahoo.com>
2022-02-19 19:00:16 -0600

    Add landing page
    - Simple welcome/about message
    - Button to handle login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold"> Hello world! </h1>;
+  return (
+    <div className="grid place-items-center h-screen bg-gray-100">
+      <div className="text-center flex flex-col space-y-4">
+        <h1 className="text-4xl font-semibold">Suggestify</h1>
+        <p className="text-xl text-gray-500">
+          Get suggestions based on your listening habits
+        </p>
+        <div className="py-3">
+          <a
+            href="#"
+            className="py-3 px-6 font-semibold rounded-md bg-emerald-500 text-white hover:bg-emerald-600"
+          >
+            Log In
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
