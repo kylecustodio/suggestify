570f512cc6885a7620ede1bd708444cf32f0058a
Kyle Custodio <kkzyc@yahoo.com>
2022-05-16 17:08:06 -0500

    Format followers
    - Following instagram style truncation (i.e. 1m, 1.2k, 100, etc)

--- components/artistTile.tsx
+++ components/artistTile.tsx
@@ -7,6 +7,23 @@ interface ArtistTileProps {
 }
 
 const ArtistTile: FC<ArtistTileProps> = ({ artist }) => {
+  const pow = Math.pow,
+    floor = Math.floor,
+    abs = Math.abs,
+    log = Math.log;
+  const abbrev = "kmb"; // could be an array of strings: [' m', ' Mo', ' Md']
+
+  const round = (n: number) => {
+    return Math.round(n * 10) / 10;
+  };
+
+  const formatFollowers = (n: number): string => {
+    var base = floor(log(abs(n)) / log(1000));
+    var suffix = abbrev[Math.min(2, base - 1)];
+    base = abbrev.indexOf(suffix) + 1;
+    return suffix ? round(n / pow(1000, base)) + suffix : "" + n;
+  };
+
   return (
     <li className="py-3">
       <div className="grid grid-cols-6 gap-x-1 items-center">
@@ -28,7 +45,7 @@ const ArtistTile: FC<ArtistTileProps> = ({ artist }) => {
         </div>
         <div className="col-span-2 invisible lg:visible">
           <div className="text-md font-semibold text-slate-500 text-right">
-            {artist.followers.total.toLocaleString() + " Followers"}
+            {formatFollowers(artist.followers.total) + " Followers"}
           </div>
         </div>
       </div>
