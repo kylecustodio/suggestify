f926690caaed9d716213b9c65b2e640af2940c49
Kyle Custodio <kkzyc@yahoo.com>
2022-05-16 19:14:19 -0500

    Add footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="mx-auto text-center py-4 text-sm">
+      <div className="text-gray-500">Made by Kyle Custodio</div>
+    </div>
+  );
+};
+
+export default Footer;
