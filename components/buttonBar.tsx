df8715129e199cf982fe7065e82630978429eb5c
Kyle Custodio <kkzyc@yahoo.com>
2022-05-22 16:54:31 -0500

    Add button bar component

---
+++ components/buttonBar.tsx
@@ -0,0 +1,33 @@
+import { RadioGroup } from "@headlessui/react";
+import { Dispatch } from "react";
+
+type ButtonBarProps = {
+  value: any;
+  setValue: Dispatch<any>;
+  options: any[];
+};
+
+const ButtonBar: React.FC<ButtonBarProps> = ({ value, setValue, options }) => {
+  return (
+    <RadioGroup value={value} onChange={setValue}>
+      {/* <RadioGroup.Label>test</RadioGroup.Label> */}
+      <div className="flex space-x-2">
+        {options.map((option) => (
+          <RadioGroup.Option
+            key={option.value}
+            value={option}
+            className={({ checked }) =>
+              `
+              ${checked ? "bg-emerald-500 text-white border-none" : "bg-white"}
+              rounded-md py-2 px-4 font-semibold border`
+            }
+          >
+            <div>{option.display}</div>
+          </RadioGroup.Option>
+        ))}
+      </div>
+    </RadioGroup>
+  );
+};
+
+export default ButtonBar;
